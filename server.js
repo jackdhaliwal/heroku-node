3357707f108cf1b95dbd999d531a0890684d1b8b
Brinder Dhaliwal <brinder.dhaliwal13@gmail.com>
2017-07-09 09:52:28 -0700

    adding node version to package.json

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var port = process.env.PORT || 8080;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.set('views', __dirname + 'views');
-
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
